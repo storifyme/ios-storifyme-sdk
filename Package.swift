20f40bb421d79fe8d446989fc14feab96c706d61
StorifyMe Team <139544247+storifyme-team@users.noreply.github.com>
2025-01-21 11:33:22 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+// swift-tools-version:5.3
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StorifyMe</title>
-</head>
+import PackageDescription
 
-<body>
-</body>
-
-</html>
+let package = Package(
+    name: "StorifyMe",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "StorifyMe",
+            targets: ["StorifyMe"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "StorifyMe",
+            url: "https://sdk.storifyme.com/ios/2.4.8/StorifyMe.zip",
+            checksum: "dd80f908ae0cc421491ce81492df98a990d33d70304c74aaf3a66db0cfe5d1f9"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
