93da302be8efcebd344e124aa82de79e94c498b9
StorifyMe Team <contact@storifyme.com>
2025-07-04 10:28:25 +0000

    Version 2.4.17

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "StorifyMe",
-            url: "https://sdk.dev.storifyme.com/ios/2.4.16/StorifyMe.zip",
-            checksum: "ddf73dd6515a81f9621988c0a97dc2924d241260009be6b9a62eb8151b23ee81"
+            url: "https://sdk.dev.storifyme.com/ios/2.4.17/StorifyMe.zip",
+            checksum: "07c9d3b8e212db3c35a41f7f25c6bf31287577e6bb3109fb4cf748a2001a40de"
         )
     ],
     swiftLanguageVersions: [.v5]
